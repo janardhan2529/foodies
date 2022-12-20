83acbc42c6e3f4b4612e81c239cb3fd9e6d2cfb6
janardhan2529 <114227814+janardhan2529@users.noreply.github.com>
2022-12-20 08:52:20 +0530

    add
    added

---
+++ shell.sh
@@ -0,0 +1,32 @@
+ #!/bin/bash
+ sudo apt update -y
+ function installjdk(){
+    sudo apt install -y openjdk-11-jdk
+ }
+    function checkDirectory(){
+        if [ -d /u02/middleware ]; then
+            echo "directory already available current agent node"
+        else
+         echo "directory not available pls create and go forther"
+          mkdir -p /u02/middleware
+         sudo chown -R vagrant:vagrant /u02/middleware
+
+        fi
+    }
+function checkTomcatServer(){
+    CHECK_TOMCAT=$(cat /etc/passwd | cut -d ':' -f1 | grep 'tomcat')
+    if [[ $CHECK_TOMCAT == *"tomcat"* ]]; then
+    echo "user already existed"
+    else
+     echo "create user"
+        sudo useradd -m -s /bin/bash tomcat
+        fi
+ }
+ function tomcatInstall(){
+    sudo su tomcat bash -c "wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.70/bin/apache-tomcat-9.0.70-fulldocs.tar.gz -o /u02/middleware/apache-tomcat-9.0.70-fulldocs.tar.gz"
+
+ }
+ installjdk
+ checkDirectory
+ checkTomcatServer
+ tomcatInstall
